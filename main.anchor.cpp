8b641fdccc538f11b7daed571d1ebe84d898042b
Federico Amedeo Izzo <federico.izzo42@gmail.com>
2017-11-30 12:07:07 +0100

    First ranging experiment

--- main.anchor.cpp
+++ main.anchor.cpp
@@ -1,9 +1,11 @@
 
 #include <cstdio>
 #include "miosix.h"
+#include <interfaces-impl/vht.h>
 #include <interfaces-impl/transceiver.h>
 #include <interfaces-impl/transceiver_timer.h>
 #include <interfaces-impl/power_manager.h>
+#define DELAY 1000000 //in ns = 1ms
 
 using namespace std;
 using namespace miosix;
@@ -11,35 +13,36 @@ using namespace miosix;
 int main()
 {
     //auto& power_manager = PowerManager::instance();
+    TimeConversion tc(EFM32_HFXO_FREQ);
+    auto& vht = VHT::instance();
     auto& transceiver = Transceiver::instance();
-    transceiver.configure(TransceiverConfiguration());
+    transceiver.configure(TransceiverConfiguration(2450,5)); //5dBm
     transceiver.turnOn();
 
     unsigned char ID = 1;
-    long long int wait_time = 1000000;
+    long long int wait_time = tc.ns2tick(DELAY); //1ms
 
     unsigned char recvpkt[125];
-
-    long long int start_ts = 0;
 
     RecvResult result;
 
     printf("I am an anchor, my ID is %d\n", ID);
     printf("ready to receive\n");
 
+    vht.stopResyncSoft();
     while (true) {
         try {
-            result = transceiver.recv(recvpkt, sizeof(recvpkt), infiniteTimeout);
+            result = transceiver.recv(recvpkt, sizeof(recvpkt), infiniteTimeout, Transceiver::Unit::TICK, HardwareTimer::Correct::CORR);
             ledOn();
         } catch (exception& e) {
             printf("Exception: %s\n", e.what());
         }
         if(recvpkt[0] == ID) {
-            auto send_time = result.timestamp + wait_time;
-            transceiver.sendAt(recvpkt, result.size, send_time);
+            long long int send_time = result.timestamp + wait_time;
+            transceiver.sendAt(recvpkt, result.size, send_time, Transceiver::Unit::TICK);
             ledOff();
             printf("Received packet @ %lld\n", result.timestamp);
-            printf("Replying after %lld\n", wait_time);
+            printf("Replying after %lld\n ticks", wait_time);
         }
     }
 }
--- main.node.cpp
+++ main.node.cpp
@@ -1,11 +1,14 @@
 
 #include <cstdio>
 #include "miosix.h"
+#include <interfaces-impl/vht.h>
 #include <interfaces-impl/transceiver.h>
 #include <interfaces-impl/transceiver_timer.h>
 #include <interfaces-impl/power_manager.h>
 #define ANCHORS 1
-#define SLEEP 500
+#define SLEEP 50
+#define ANCH_DELAY 1000000 //in ns = 1ms
+#define RECV_TIMEOUT 10000000 //in ns = 10ms
 
 using namespace std;
 using namespace miosix;
@@ -13,38 +16,55 @@ using namespace miosix;
 int main()
 {
     //auto& power_manager = PowerManager::instance();
+    TimeConversion tc(EFM32_HFXO_FREQ);
+    auto& vht = VHT::instance();
     auto& transceiver = Transceiver::instance();
-    transceiver.configure(TransceiverConfiguration());
+    transceiver.configure(TransceiverConfiguration(2450,5)); //5dBm
     transceiver.turnOn();
 
     unsigned char sendpkt[10];
     memset(sendpkt,0,sizeof(sendpkt));
     unsigned char recvpkt[125];
 
-    long long int start_ts = 0;
+    long long int start_ns = 0, start_tick = 0;
+    long long int anchor_delay = tc.ns2tick(ANCH_DELAY);
 
     RecvResult result;
+
+    float tof = 0.f;
+    const float alpha = 0.9f;
+    vht.stopResyncSoft();
 
     while (true) {
         for(int i=1; i<=ANCHORS; i++)
         {
             // Send packet
+            printf("Sending to anchor %d. ", i);
             sendpkt[0] = i;  //select anchor saving the number in the payload
-            start_ts = getTime();
-            start_ts += 30000000;
-            transceiver.sendAt(sendpkt, sizeof(sendpkt), start_ts, Transceiver::Unit::NS);
-            //printf("packet sent to anchor %c @ %lld\n", i, start_ts);
+            start_ns = getTime();
+            start_tick = tc.ns2tick(start_ns);
+            start_tick += 30000000;
+            //vht.stopResyncSoft();
+            transceiver.sendAt(sendpkt, sizeof(sendpkt), start_tick, Transceiver::Unit::TICK);
             ledOn();
             try {
-                result = transceiver.recv(recvpkt, sizeof(recvpkt), infiniteTimeout, Transceiver::Unit::NS);
+                result = transceiver.recv(recvpkt, sizeof(recvpkt), start_tick+tc.ns2tick(RECV_TIMEOUT), Transceiver::Unit::TICK);
+                //vht.startResyncSoft();
                 ledOff();
             } catch (exception& e) {
                 printf("Exception: %s\n", e.what());
             }
-            if(recvpkt[0] == i && result.timestampValid)
+            if(recvpkt[0] == i && result.timestampValid && result.error==RecvResult::OK)
             {
-                printf("received response @ %lld\n", result.timestamp);
-                printf("delta = %lld\n", result.timestamp - start_ts);
+                printf("received response, rtt=%lld, ", result.timestamp - start_tick - anchor_delay);
+                tof = alpha * tof + (1.0f - alpha) * max(static_cast<float>(result.timestamp - start_tick - anchor_delay)/2.0f, 0.f);
+                printf("tof (tick)=%5.1f (m)=%5.1f, ", tof, tof / static_cast<float>(EFM32_HFXO_FREQ) * 299702561);
+                printf("rssi=%d\n", result.rssi);
+                //299702561 m/s
+            }
+            else if(result.error!=RecvResult::OK)
+            {
+                printf("receive error: %d\n", result.error);
             }
             else if(!result.timestampValid)
             {
@@ -54,7 +74,7 @@ int main()
             {
                 printf("wrong response\n");
             }
-            memDump(recvpkt,result.size);
+            //memDump(recvpkt,result.size);
         }
     Thread::sleep(SLEEP);
     }
